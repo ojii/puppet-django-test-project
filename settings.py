6f72b8778875d03a6889700437f5bc2e4b37d473
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-28 18:38:36 +0200

    initial commit

---
+++ settings.py
@@ -0,0 +1,74 @@
+# Django settings for src project.
+import os
+
+DEBUG = TEMPLATE_DEBUG = False
+APPEND_SLASH = True
+
+ADMINS = MANAGERS = []
+
+TIME_ZONE = 'Europe/Zurich'
+
+LANGUAGE_CODE = 'en'
+
+SITE_ID = 1
+
+USE_L10N = USE_I18N = False
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+SECRET_KEY = 'super.secret,this.is.an.example,dont.use.this'
+
+TEMPLATE_LOADERS = [
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+]
+
+TEMPLATE_DIRS = [
+    os.path.join(PROJECT_DIR, 'templates'),
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF = 'project.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    'south',
+    'project',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.request",
+    "django.core.context_processors.static",
+    "django.core.context_processors.request",
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'database.sqlite',
+    }
+}
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
